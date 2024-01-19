544ca88bdf7900a8255f8f5119e89e395dfbb647
bmokr <56951744+bmokr@users.noreply.github.com>
2024-01-19 16:48:29 +0100

    v0.5
    -login register started

--- task_manager/listings/urls.py
+++ task_manager/listings/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('logged_home', views.logged_home, name='logged_home'),
     path('<int:listing_id>', views.listing, name='listing'),
     path('<int:listing_id>/history/', history_view, name='history_view'),
-    path('search', views.search, name='search'),
 
 ]
--- task_manager/mng/settings.py
+++ task_manager/mng/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'simple_history',
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
