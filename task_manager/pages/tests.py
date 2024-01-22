11653f1c8d5f6e9dafc17febadec8363f0dcc190
bmokr <56951744+bmokr@users.noreply.github.com>
2024-01-22 21:07:48 +0100

    v0.13
    - pages views/urls tests

--- task_manager/pages/tests.py
+++ task_manager/pages/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from pages.views import index, about, login, register
 
-# Create your tests here.
+
+class TestUrls(SimpleTestCase):
+
+    def test_index_url_is_resolved(self):
+        url = reverse('index')
+        self.assertEqual(resolve(url).func, index)
+
+    def test_about_url_is_resolved(self):
+        url = reverse('about')
+        self.assertEqual(resolve(url).func, about)
+
+    def test_login_url_is_resolved(self):
+        url = reverse('login')
+        resolved_func = resolve(url).func
+        self.assertEqual(resolved_func.__name__, 'login')
+
+    def test_register_url_is_resolved(self):
+        url = reverse('register')
+        resolved_func = resolve(url).func
+        self.assertEqual(resolved_func.__name__, 'register')
